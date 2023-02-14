a4f5e512f0323fc3a8bf1ff8c5112a422cb850c3
Desy Ayurianti <desii.ayurianti@gmail.com>
2023-02-14 15:15:31 +0700

    edit

--- projectWPLP1/resources/views/welcome.blade.php
+++ projectWPLP1/resources/views/welcome.blade.php
@@ -19,8 +19,19 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+
+
     </head>
     <body class="antialiased">
+        <div class = "relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+            <div class = "ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-988 dark:text-white">Desy Ayurianti</h1>
+
+            </div>
+        </div>
+
+        
+    </body>
+    <!-- <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
@@ -128,5 +139,5 @@
                 </div>
             </div>
         </div>
-    </body>
+    </body> -->
 </html>
